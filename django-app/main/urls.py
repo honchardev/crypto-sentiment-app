c85b1c5445ae9f7e606c29382f188453d070a11a
Maksym Honchar <honchardev@gmail.com>
2019-05-31 08:28:47 +0300

    Add basic views and templates for all types of pages; add login/logout functionality

--- django-app/main/urls.py
+++ django-app/main/urls.py
@@ -4,6 +4,17 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('currencies/', views.currencies, name='currencies'),
+    path('market/', views.market, name='market'),
+    path('news/', views.news, name='news'),
+    path('dev/', views.dev, name='dev'),
+    path('userguide/', views.userguide, name='userguide'),
+    path('devguide/', views.devguide, name='devguide'),
+    path('apppurpose/', views.apppurpose, name='apppurpose'),
+    path('aboutalgo/', views.aboutalgo, name='aboutalgo'),
+    path('signup/', views.signup, name='signup'),
+
     path('api/', views.api_index, name='api_index'),
     path('api/getcurreqscrapedtweetscnt', views.api_getcurreqscrapedtweetscnt, name='api_getcurreqscrapedtweetscnt'),
 ]
--- django-app/main/views.py
+++ django-app/main/views.py
@@ -1,10 +1,82 @@
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.http import JsonResponse
+from django.shortcuts import redirect, render
 from django.utils import timezone
+from django.http import HttpResponse
 
 from .scrapers.scrape_twitter import TwitterScraper
 
 
 twitter_scrapper = TwitterScraper()
+
+
+def index(req):
+    return render(req, 'index.html')
+
+
+def currencies(req):
+    context = {
+        'currencies_page_active': True
+    }
+    return render(req, 'currencies.html', context=context)
+
+
+def market(req):
+    context = {
+        'market_page_active': True
+    }
+    return render(req, 'market.html', context=context)
+
+
+def news(req):
+    context = {
+        'news_page_active': True
+    }
+    return render(req, 'news.html', context=context)
+
+
+@login_required
+def dev(req):
+    return render(req, 'dev.html')
+
+
+def userguide(req):
+    return render(req, 'userguide.html')
+
+
+def devguide(req):
+    return render(req, 'devguide.html')
+
+
+def apppurpose(req):
+    return render(req, 'apppurpose.html')
+
+
+def aboutalgo(req):
+    return render(req, 'aboutalgo.html')
+
+
+@login_required
+def signup(req):
+    if not req.user.is_superuser:
+        return redirect('index')
+    context = {}
+    if req.method == 'POST':
+        form = UserCreationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            context['form'] = form
+            context['status'] = True
+            context['new_user_username'] = form.cleaned_data.get('username')
+        else:
+            context['status'] = False
+    else:
+        form = UserCreationForm()
+        context['form'] = form
+    return render(req, 'signup.html', context=context)
 
 
 def api_index(req):
